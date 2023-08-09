c2f98e3fdac7ad85040397c7ee412439871d2141
CCNL-UniPD <119981330+CCNL-UniPD@users.noreply.github.com>
2023-08-09 15:48:33 +0200

    Add files via upload

---
+++ load_figure_disconn.py
@@ -0,0 +1,19 @@
+import gl
+import sys
+print(sys.version)
+print(gl.version())
+gl.resetdefaults()
+#open background image
+gl.loadimage('spm152')
+## Smooth interpolation of overlay 
+gl.overlayloadsmooth(1)
+#open disconnectome overlay
+gl.overlayload("G:/.shortcut-targets-by-id/1du-K1whFNz4F791pBOvFGNOe-EBoIxzN/Articolo LOAD/mappe e immagini di prova/disconnectome_map_z.nii")
+# set min to threshold for visualization
+gl.minmax(1, 1.5, 5.03177)
+gl.opacity(1,70)
+gl.colorname(1, '4hot')
+gl.colorbarposition(1)
+gl.colorbarsize(0.03)
+# set mosaic slices for disconnectome hot spots
+gl.mosaic("S A L- H -0.1 V -0.05  -12 -8 5 15 20 30 35; C -45 -35 -25 -10 -3 2 10");
